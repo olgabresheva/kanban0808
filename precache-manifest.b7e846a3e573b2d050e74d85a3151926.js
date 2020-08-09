83c268499f1eb427b6007fb2a69a18c69b3f39dd
olgab <olgabresheva7@gmail.com>
2020-08-08 23:48:38 -0700

    Updates

---
+++ precache-manifest.b7e846a3e573b2d050e74d85a3151926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8acc9f624de08ae3eb62dcb8649b0f2",
+    "url": "/kanban0808/index.html"
+  },
+  {
+    "revision": "c6f476bcfabbe34a2bef",
+    "url": "/kanban0808/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "37b29e58c2d07183c178",
+    "url": "/kanban0808/static/css/main.99e54aa4.chunk.css"
+  },
+  {
+    "revision": "c6f476bcfabbe34a2bef",
+    "url": "/kanban0808/static/js/2.1640c64d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kanban0808/static/js/2.1640c64d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37b29e58c2d07183c178",
+    "url": "/kanban0808/static/js/main.abe2650f.chunk.js"
+  },
+  {
+    "revision": "413c3b8b22187cb73309",
+    "url": "/kanban0808/static/js/runtime-main.d6a70ad2.js"
+  }
+]);
